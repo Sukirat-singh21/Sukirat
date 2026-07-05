44db5859bfcfc015688fca9f23c3d07eeeeac2d0
Sukirat-singh21 <suratgaming21@gmail.com>
2026-07-06 01:55:51 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'jee-pomodoro-flow-v13';
+const CACHE = 'jee-pomodoro-flow-v14';
 const ASSETS = ['./', 'index.html', 'style.css', 'app.js', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', event => {
